e52963c34fdeafbaf1060bbe644b9a6ab7a7bca1
sabbir1054 <mdsabbir1054@gmail.com>
2023-09-29 08:41:59 +0600

    setup environment with starter file

---
+++ README.md
@@ -0,0 +1,3 @@
+# University Management Core Services
+
+---
---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { Server } from 'http';
+import app from './app';
+import config from './config';
+import { errorlogger, logger } from './shared/logger';
+
+/* university-management-core-services */
+
+async function bootstrap() {
+  const server: Server = app.listen(config.port, () => {
+    logger.info(`Server running on port ${config.port}`);
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        logger.info('Server closed');
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    errorlogger.error(error);
+    exitHandler();
+  };
+
+  process.on('uncaughtException', unexpectedErrorHandler);
+  process.on('unhandledRejection', unexpectedErrorHandler);
+
+  process.on('SIGTERM', () => {
+    logger.info('SIGTERM received');
+    if (server) {
+      server.close();
+    }
+  });
+}
+
+bootstrap();
